6ba41a8a016dbdc3f93e4ed888d8d1179e8a4e1d
Christian Gama <christiangama.dev@gmail.com>
2023-03-22 13:53:57 -0300

    test: ensure update_college controller works correctly

---
+++ internal/presentation/controller/college/update_test.go
@@ -0,0 +1,93 @@
+package controller_test
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/christian-gama/pd-solucoes/internal/app/dto"
+	controller "github.com/christian-gama/pd-solucoes/internal/presentation/controller/college"
+	fake "github.com/christian-gama/pd-solucoes/testutils/fake/app/dto"
+	"github.com/christian-gama/pd-solucoes/testutils/gintest"
+	mocks "github.com/christian-gama/pd-solucoes/testutils/mocks/app/service/college"
+	"github.com/christian-gama/pd-solucoes/testutils/suite"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type UpdateCollegeSuite struct {
+	suite.Suite
+}
+
+func TestUpdateCollegeSuite(t *testing.T) {
+	suite.RunUnitTest(t, new(UpdateCollegeSuite))
+}
+
+func (s *UpdateCollegeSuite) TestHandle() {
+	type Sut struct {
+		Sut           controller.UpdateCollege
+		Input         *dto.UpdateCollegeInput
+		UpdateCollege *mocks.UpdateCollege
+	}
+
+	makeSut := func() *Sut {
+		input := fake.UpdateCollegeInput()
+		updateCollege := new(mocks.UpdateCollege)
+		sut := controller.NewUpdateCollege(updateCollege)
+		return &Sut{Sut: sut, UpdateCollege: updateCollege, Input: input}
+	}
+
+	s.Run("should update a college", func() {
+		sut := makeSut()
+
+		sut.UpdateCollege.On("Handle", mock.Anything, sut.Input).
+			Return(&dto.UpdateCollegeOutput{}, nil)
+
+		ctx := gintest.MustRequest(sut.Sut, gintest.Option{
+			Data:   sut.Input,
+			Params: []string{fmt.Sprint(sut.Input.ID)},
+		})
+
+		s.Equal(http.StatusOK, ctx.Writer.Status())
+		sut.UpdateCollege.AssertCalled(s.T(), "Handle", mock.Anything, sut.Input)
+	})
+
+	s.Run("invalid Name: it's required", func() {
+		sut := makeSut()
+
+		sut.Input.Name = ""
+
+		ctx, _ := gintest.MustRequestWithBody(sut.Sut, gintest.Option{
+			Data:   sut.Input,
+			Params: []string{fmt.Sprint(sut.Input.ID)},
+		})
+
+		s.Equal(http.StatusBadRequest, ctx.Writer.Status())
+	})
+
+	s.Run("invalid Name: max length", func() {
+		sut := makeSut()
+
+		sut.Input.Name = strings.Repeat("a", 101)
+
+		ctx, _ := gintest.MustRequestWithBody(sut.Sut, gintest.Option{
+			Data:   sut.Input,
+			Params: []string{fmt.Sprint(sut.Input.ID)},
+		})
+
+		s.Equal(http.StatusBadRequest, ctx.Writer.Status())
+	})
+
+	s.Run("panics when UpdateCollege.Handle returns error", func() {
+		sut := makeSut()
+
+		sut.UpdateCollege.On("Handle", mock.Anything, sut.Input).Return(nil, assert.AnError)
+
+		s.Panics(func() {
+			gintest.MustRequest(sut.Sut, gintest.Option{
+				Data: sut.Input,
+			})
+		})
+	})
+}
