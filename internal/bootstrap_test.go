f1bec905588a60ab8f82cbb4b7afff6088cf8c1e
Christian Gama <christiangama.dev@gmail.com>
2023-03-23 19:14:14 -0300

    fix: bootstrap log mock

--- internal/bootstrap_test.go
+++ internal/bootstrap_test.go
@@ -22,7 +22,9 @@ func TestBootstrapSuite(t *testing.T) {
 func (s *BootstrapSuite) TestBootstrap() {
 	s.Run("do not panic", func() {
 		log := mocks.NewLogger(s.T())
-		log.On("Infof", mock.Anything, mock.Anything).Times(3)
+		log.On("Infof", mock.Anything, mock.Anything)
+		log.On("Infof", mock.Anything, mock.Anything, mock.Anything)
+		log.On("Infof", mock.Anything, mock.Anything)
 		log.On("Fatal", mock.Anything).Maybe()
 
 		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Nanosecond)
